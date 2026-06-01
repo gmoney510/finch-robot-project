9e92610b11d1d5d73507a7c96d88a3542eec4b70
gmoney510 <gmilitel0003@mymail.lausd.net>
2026-06-01 13:15:24 -0700

    jhg

--- src/main/java/com/luca/FinalFinch.java
+++ src/main/java/com/luca/FinalFinch.java
@@ -9,5 +9,50 @@ public class FinalFinch extends Finch{
     }
 
     //write custom methods
+    public void paintAllHouses() {
+        paintOneHouse();
+        // write code to get to next house
+        setTurn("R", 90, 50);
+        setMove("F", 1.75, 100);
+        setMove("F", 3, 100);
+        setTurn("R", 90, 50);
+        setMove("F", 5, 100);
+        setTurn("L", 180, 50);
+
+
+
+    }
+
+    public void paintOneHouse() {
+        //Square
+        setMove("F", 5, 100);
+        setTurn("R", 90, 50);
+        setMove("F", 5, 100);
+        setTurn("R", 90, 50);
+        setMove("F", 5, 100);
+        setTurn("R", 90, 50);
+        setMove("F", 5.5, 100);
+        //triangle
+        setMove("B", 5.5, 100);
+        setTurn("L", 45, 50);
+        setMove("F", 4, 100);
+        setTurn("R", 95, 50);
+        setMove("F", 4, 100);
+
+        //door
+         setTurn("R", 45, 50);
+         setMove("F", 5, 100);
+         setTurn("R", 90, 50);
+         setMove("F", 1.70, 100);
+         setTurn("R", 90, 50);
+         setMove("F", 2.5, 100);
+         setTurn("L", 90, 50);
+         setMove("F", 1.60, 100);
+         setTurn("L", 90, 50);
+         setMove("F", 2.67, 100);
+    }
+
+
+    
     
 }
--- src/main/java/com/luca/TesterFinch.java
+++ src/main/java/com/luca/TesterFinch.java
@@ -11,20 +11,9 @@ public class TesterFinch{
 
         bird.setBeak(100, 50, 60);
 
-        //Square
-        bird.setMove("F", 5, 100);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 5, 100);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 5, 100);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 5.25, 100);
-        //triangle
-        bird.setMove("B", 5.5, 100);
-        bird.setTurn("L", 45, 50);
-        bird.setMove("F", 4, 100);
-        bird.setTurn("R", 105, 50);
-        bird.setMove("F", 4, 100);
+        bird.paintAllHouses();
+
+
 
        
         
